66cbf71890ef49a41036ad9232fa851bad81d045
lostsh <0pdlna3@gmail.com>
2020-09-26 14:20:13 +0200

    Konami code detection kork pretty good
    the code detection system is okay, so now going to create a glitch effect !

--- assets/js/konami.js
+++ assets/js/konami.js
@@ -1,39 +1,57 @@
-let keySeq = ["","","","","","","","","",""];
+let keySeq = new Array();
+function clrTab(){for(i in keySeq+1){keySeq.pop();}}
 
 // Key pressed detection
 document.onkeydown = keys;
 function keys(objectHandler){
-    console.log("[+] Pressed "+objectHandler.key+"\n");
+    //console.log("[+] Pressed "+objectHandler.key+"\n");
 
     keySeq.push(objectHandler.key);
-    detectCodeKonami();
-    /*
-    switch (objectHandler.key) {
-        case 'ArrowUp':
-            console.log("[+] Pressed "+objectHandler.key+"\n");
-            break;
-        case 'ArrowDown':
-            console.log("[+] Pressed "+objectHandler.key+"\n");
-            break;
-        case 'ArrowLeft':
-            console.log("[+] Pressed "+objectHandler.key+"\n");
-            break;
-        case 'ArrowRight':
-            console.log("[+] Pressed "+objectHandler.key+"\n");
-            break;
-        case 'a':
-            console.log("[+] Pressed "+objectHandler.key+"\n");
-            break;
-        case 'b':
-            console.log("[+] Pressed "+objectHandler.key+"\n");
-            break;
-    }*/
+    if(detectCodeKonami(keySeq) == 0){
+        displayGlitch();
+    }
+    //console.log(keySeq);
 }
 
-function main(){
 
+/**
+ * Return 0 if the konami code was correctly typed else return 1
+ * @param {*} keyTab tab of key sequance
+ */
+function detectCodeKonami(keyTab){
+    if(isValide(keyTab)==0){//if the key seq correspond with konami code
+        if(keyTab.length == 10){
+            //console.log("Okey then the code is valide !");
+            clrTab();
+            return 0;
+        }else{
+            //console.log("The code isn't complete but not wrong yet");
+        }
+    }else{//when the sequance of key do not correspond to the code clear the tab
+        //console.log("WRONG Clear the tab");
+        clrTab();
+    }
+    return 1;
 }
 
-function detectCodeKonami(){
+/**
+ * Return 1 if the table contain a wrong key
+ * and return 0 if all the keys in the tab are valide (even if the tab is not complette)
+ * @param {*} tabOfKeys Key tab
+ */
+function isValide(tabOfKeys){
+    let valideSeq = new Array("ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a");
+    for(var i=0;i<valideSeq.length;i++){
+        if(valideSeq[i]!=tabOfKeys[i] && tabOfKeys[i]!="" && tabOfKeys[i]!=null){
+            return 1;
+        }
+    }
+    return 0;
+}
 
+
+
+
+function displayGlitch(){
+    document.write("Coucou toi ~");
 }
